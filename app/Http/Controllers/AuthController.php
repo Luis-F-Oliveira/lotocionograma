599e12878ea262e41741fe2a61caaee5aa6f8566
Seu Nome <seu-email@example.com>
2023-09-22 17:37:08 -0400

    autenticacao quase completa

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,20 +5,38 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request) 
     {
-        $credentials = $request->only('email', 'password');
+        try {
+            $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $user = $request->user();
-            $token = $user->createToken('authToken')->plainTextToken;
+            if ($credentials) {
+                if (Auth::attempt($credentials)) {
+                    return response()->json(['credenciais' => $credentials]);
+                }
+            }
 
-            return response()->json(['user' => $user, 'token' => $token]);
+        //             return response()->json(['entrei']);
+
+        //         return response()->json([$credentials]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Ocorreu um erro durante o login', 'message' => $e->getMessage()], 500);
         }
-
-        return response()->json(['message' => 'Credenciais inválidas'], 401);
     }
 }
+
+// $credentials = $request->only('email', 'password');
+
+// if (Auth::attempt($credentials)) {
+//     $user = $request->user();
+//     $token = $user->createToken('authToken')->plainTextToken;
+
+//     return response()->json(['user' => $user, 'token' => $token]);
+// }
+
+// return response()->json(['message' => 'Credenciais inválidas'], 401);
