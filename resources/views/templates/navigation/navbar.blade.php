ade6ca21ae86773ad4a6ac064761de2e93dcfdb0
Seu Nome <seu-email@example.com>
2023-09-29 17:21:23 -0400

    mudança na navbar

--- resources/views/templates/navigation/navbar.blade.php
+++ resources/views/templates/navigation/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar bg-light mb-1">
-    <div class="container-fluid d-flex justify-content-end">
-        <div class="d-flex align-items-center">
-            <img class="perfil-img rounded-circle" src="{{ asset('img/user/admperfil.jpg') }}" alt="user-perfil-img">
-            <i id="config-button" class="fa-solid fa-gear fs-5 ms-3 config"></i>
-        </div>
+    <div class="container-fluid d-flex justify-content-beetween">
+        <a class="navbar-brand text-primary fw-bold" href="">
+            Lotacionograma
+        </a>
+        <i id="config-button" class="fa-solid fa-gear fs-5 ms-3 config"></i>
     </div>
 </nav>
 
